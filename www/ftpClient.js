f3b448f96f31b5eebae324bd8490be6120b75d64
Davide Pierfederici <DAVIDEP@pluservice.local>
2016-02-29 11:30:39 +0100

    ADD external libs

--- www/ftpClient.js
+++ www/ftpClient.js
@@ -1,41 +1,36 @@
-cordova.define(
-	"cordova-plugin-ftp-client.FtpClient",
-	function(require, exports, module) {
-		var exec = require("cordova/exec");
+var exec = require("cordova/exec");
 
-		function FtpClient() {
-		}
+function FtpClient() {
+}
 
-		FtpClient.prototype.get = function (options, successCallback, errorCallback) {
-			return exec(
-				successCallback,
-				errorCallback,
-				"FtpClient",
-				"get",
-				[
-					options.url,
-					options.remote,
-					options.local,
-					options.credentials
-				]
-			);
-		};
+FtpClient.prototype.get = function (options, successCallback, errorCallback) {
+	return exec(
+		successCallback,
+		errorCallback,
+		"FtpClient",
+		"get",
+		[
+			options.url,
+			options.remote,
+			options.local,
+			options.credentials
+		]
+	);
+};
 
-		FtpClient.prototype.put = function (options, successCallback, errorCallback) {
-			return exec(
-				successCallback,
-				errorCallback,
-				"FtpClient",
-				"put",
-				[
-					options.url,
-					options.remote,
-					options.local,
-					options.credentials
-				]
-			);
-		};
+FtpClient.prototype.put = function (options, successCallback, errorCallback) {
+	return exec(
+		successCallback,
+		errorCallback,
+		"FtpClient",
+		"put",
+		[
+			options.url,
+			options.remote,
+			options.local,
+			options.credentials
+		]
+	);
+};
 
-		module.exports = new FtpClient();
-	}
-);
+module.exports = new FtpClient();
